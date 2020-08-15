de2fed5cb6840297f1fd430a0814c47d07c9fa71
jeeeeeeeeee <noqtee@gmail.com>
2020-08-15 11:34:25 +0300

    commit #1

---
+++ config.py
@@ -0,0 +1,18 @@
+"""
+doc string goes here
+"""
+
+__all__ = ['']
+
+# Standard library imports.
+
+# Related third party imports.
+
+# Local application/library specific imports.
+
+
+# Create your models here.
+
+TOKEN = ''  # Bot-token
+TIMEZONE = 'Europe/Kiev'
+TIMEZONE_COMMON_NAME = 'Kiev'
